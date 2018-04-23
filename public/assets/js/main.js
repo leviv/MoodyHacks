6a5aceb6c5462b0214723beabbd6f373d77b3dac
Levi V <villarreallevi@gmail.com>
2018-04-22 23:55:30 -0500

    Update content and styles for the site

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,89 +1,144 @@
-$(document).ready(function(){
-    firebase.onAuthStateChanged(user);
-});
+// When the window has finished loading create our google map below
+google.maps.event.addDomListener(window, 'load', init);
+
+function init() {
+    // Basic options for a simple Google Map
+    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    var mapOptions = {
+        // How zoomed in you want the map to start at (always required)
+        zoom: 16,
+
+        // The latitude and longitude to center the map (always required)
+        center: new google.maps.LatLng(30.290173, -97.740993), // New York
+
+        // How you would like to style the map. 
+        // This is where you would paste any style found on Snazzy Maps.
+        styles: [{"featureType":"all","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels","stylers":[{"visibility":"off"},{"saturation":"-100"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40},{"visibility":"off"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"off"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"landscape","elementType":"geometry.stroke","stylers":[{"color":"#4d6059"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"lightness":21}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"poi","elementType":"geometry.stroke","stylers":[{"color":"#4d6059"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#7f8d89"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#2b3638"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2b3638"},{"lightness":17}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"geometry.stroke","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.icon","stylers":[{"visibility":"off"}]}]
+    };
+
+    // Get the HTML DOM element that will contain your map 
+    // We are using a div with id="map" seen below in the <body>
+    var mapElement = document.getElementById('map');
+
+    // Create the Google Map using our element and options defined above
+    var map = new google.maps.Map(mapElement, mapOptions);
+
+    // Let's also add a marker while we're at it
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(30.290173, -97.740993),
+        map: map,
+        title: 'Snazzy!'
+    });
+}
+
+
+
 
 $(document).ready(function(){
-
-    $('.switcher').click(function(e) {  
-        $(".switcher").css({"border-width": "100vh 50vw 0 0"});
+    $( "a.scrollLink" ).click(function( event ) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
     });
 });
 
 
 
-var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().languageCode = 'en';
 
-$(".sign-in").click(function() {
-	firebase.auth().signInWithPopup(provider).then(function(result) {
-	  // This gives you a Google Access Token. You can use it to access the Google API.
-	  var token = result.credential.accessToken;
-	  // The signed-in user info.
-	  var user = result.user;
-	  // ...
-	}).catch(function(error) {
-	  // Handle Errors here.
-	  var errorCode = error.code;
-	  var errorMessage = error.message;
-	  // The email of the user's account used.
-	  var email = error.email;
-	  // The firebase.auth.AuthCredential type that was used.
-	  var credential = error.credential;
-	  // ...
-	});					
-});
+/*
+    Hamburger transition tutorial
+    By Nick
+    http://callmenick.com/2015/01/25/animating-css-only-hamburger-menu-icons/
+*/
+$(document).ready(function () {
+	"use strict";
+    (function () {
+		
+		var toggles = document.querySelectorAll(".c-hamburger");
+		var i = toggles.length - 1;
+		        
+        for (i; i >= 0; i--) {
+            var toggle = toggles[i];
+            toggleHandler(toggle);
+        }
 
-var rootRef = firebase.database().ref().child('users');
+        function toggleHandler(toggle) {
+            toggle.addEventListener("click", function (e) {
+                e.preventDefault();
+                (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
 
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-	  var displayName = user.displayName;
-	  document.getElementById("yeetyeet").innerHTML = displayName;
-	  if (location.href.split(location.host)[1] !== "/dashboard.html"){
-		  rootRef.set({
-			  name:user.displayName,
-			  username:user.displayName,
-			  location: "Richardson, TX",
-			  bio:"lfucksdaf"
-		  });
-	  	  window.location.href = "dashboard.html";
-	  }
-
-  } else {
-    // No user is signed in.
-	  if (location.href.split(location.host)[1] === "/dashboard.html"){
-	  	  window.location.href = "index.html";
-	  }
-  }
+            });
+        }
+    })();
 });
 
 
+var inProgress = false;
+
+$("#nav").click(function () {
+    
+	"use strict";
+	
+	var width = $(window).width() - 68;
+	
+    if ($(".c-hamburger").hasClass("is-active")) {
+		inProgress = true;
+		$('#dim-screen').fadeTo(500, 1);
+        $("#nav-expand").animate({
+            width: "300px"
+        }, {
+            duration: 1500,
+            easing: 'easeInOutQuint'
+        });
+        $(".nav-list").animate({
+            left: "0%"
+        }, {
+            duration: 300,
+            easing: 'easeInOutQuint'
+        });
+        $(".c-hamburger").css({'transform' : 'rotate(180deg)'});
+        $("body").css("overflow", "hidden");
+    } else {
+		$('#dim-screen').fadeTo(500, 0, function (){
+			$(this).hide();
+		});
+        $("#nav-expand").animate({
+            width: "0%"
+        }, {
+            duration: 1500,
+            easing: 'easeInOutQuint'
+        });
+        $(".nav-list").animate({
+            left: "-100%"
+        }, {
+            duration: 400,
+            easing: 'easeInOutQuint'
+        });
+        $(".c-hamburger").css({'transform' : 'rotate(0deg)'});
+        $("body").css("overflow", "auto");
+    }
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(".nav-item").click(function () {
+    $(".c-hamburger").removeClass("is-active");
+    $('#dim-screen').fadeTo(500, 0, function (){
+			$(this).hide();
+		});
+        $("#nav-expand").animate({
+            width: "0%"
+        }, {
+            duration: 1500,
+            easing: 'easeInOutQuint'
+        });
+        $(".nav-list").animate({
+            left: "-100%"
+        }, {
+            duration: 400,
+            easing: 'easeInOutQuint'
+        });
+        $(".c-hamburger").css({'transform' : 'rotate(0deg)'});
+        $("body").css("overflow", "auto");
+});
 
 
 
